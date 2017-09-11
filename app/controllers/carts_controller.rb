dc8ada5298bb55a1101d2cd8653f25e8bd8ce826
emilesu <emilesu@qq.com>
2017-09-12 00:16:36 +0800

    cannot change quantity which exceed stock

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,9 @@ class CartsController < ApplicationController
   def clean
     current_cart.clean!
     flash[:warning] = "已清空回收站"
-    redirect_to carts_path
+    redirect_back(fallback_location: carts_path)
   end
+
+
 
 end
